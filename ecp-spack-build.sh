511f8c466bcb38cb8ef89640efcb16fb43d43551
Kyle Shaver <kshaver@anl.gov>
2019-04-03 12:01:04 -0500

    fixing initial spack issues

--- ecp-spack-build.sh
+++ ecp-spack-build.sh
@@ -3,6 +3,7 @@
 echo "starting build of spack-dummy-package"
 
 export HOME=/home/ecptestb
-module load spack
+#module load spack
+. /soft/spack/spack-dev/share/spack/setup-env.sh
 module load cce
-spack install spack-install-package
+spack install spack-dummy-package@ecp_spack_test
