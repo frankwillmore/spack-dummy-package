9f35137eda2d819b64800d38df386135f0510451
Frank Ted Willmore <willmore@anl.gov>
2019-04-10 21:40:59 +0000

    Update package.py

--- package.py
+++ package.py
@@ -6,7 +6,7 @@ class SpackDummyPackage(MakefilePackage):
 
     homepage = "http://www.anl.gov"
     #url      = "https://gitlab.alcf.anl.gov/willmore/spack-dummy-package.git"
-    git      = "https://gitlab.alcf.anl.gov/willmore/spack-dummy-package.git"
+    git      = "https://gitlab.alcf.anl.gov/ecp-test/spack-dummy-package.git"
 
     # version('1.2.3', '0123456789abcdef0123456789abcdef')
     version('master', branch='master')
